f2620e753bf38855d15a86082579ac9a7d94df5d
Dinesh Majrekar <dinesh@civo.com>
2024-11-26 11:01:52 +0000

    Use local dagger chart from https://github.com/DMajrekar/dagger/tree/helm-chart-secret

--- terraform/helm_dagger.tf
+++ terraform/helm_dagger.tf
@@ -3,6 +3,12 @@ resource "helm_release" "dagger_engine" {
     namespace = "dagger"
     create_namespace = true
 
-    repository = "oci://registry.dagger.io"  
-    chart = "dagger-helm"
+    # repository = "oci://registry.dagger.io"
+    # chart = "dagger-helm"
+    chart = "./chart"
+
+    set {
+        name = "envFromSecretName"
+        value = resource.kubernetes_secret.dagger_cache_config.metadata.0.name
+    }
 }
